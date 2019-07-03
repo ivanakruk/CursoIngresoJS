0615158e210d1d3ee34a5073b352dee4ac25d0dc
ivanakruk <52494488+ivanakruk@users.noreply.github.com>
2019-07-03 11:52:55 -0300

    ejercicio 4
    ....

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,20 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var numeroUno;
+	var numeroDos;
+	numeroUno= 666;
+	numeroDos= numeroUno; 
+	// numeroUno=numeroDos numeroDos=numeroUno
+
+	alert(numeroUno);
+	alert(numeroDos);
+// practicas variables numeros
+var edad;
+edad = prompt("ingrese su edad");
+document.getElementById('numero').value="edad"  
+}
+
