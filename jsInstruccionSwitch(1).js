a809b37a1c43d3d136bdd1169bb31bd020478d81
Ivana k <52494488+ivanakruk@users.noreply.github.com>
2019-07-10 12:17:58 -0300

    switch 1

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,32 @@
+function mostrar()
+{
+
+    var mesDelAño = document.getElementById('mes').value;
+
+    switch(mesDelAño)
+    {
+    case 'Enero':
+    
+    alert("Que comiences bien el año!!")
+    break;
+    
+    case 'Marzo':
+   
+    alert("a clasess!!")
+    break;
+
+    case 'Julio':
+    alert("Se vienen las vacaciones!!")
+    break;
+
+    case 'Diciembre':
+    alert("Felices Fiestas!!")
+    break;
+
+    }
+
+    
+
+
+
+}//FIN DE LA FUNCIÓN
