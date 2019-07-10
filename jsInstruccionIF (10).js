77b26d21aebed37fdd3a19312cc01fea4822d799
Ivana k <52494488+ivanakruk@users.noreply.github.com>
2019-07-10 12:12:02 -0300

    if 10

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,23 @@
+function mostrar()
+{
+	var numero;
+	numero=parseInt(numero);	
+	numero=Math.floor(Math.random(11-1)*10)+1;
+	
+	if(numero>=9){
+		alert("Su nota es: "+numero+ " Excelente");
+	}
+	else if(numero>=4){
+		alert("Usted se saco una nota de : "+numero+" APROBÓ");
+	}
+	else
+	{
+	alert("Su nota es: "+numero+" Vamos,  la proxima se puede");
+	}
+	
+
+
+	//Genero el número RANDOM entre 1 y 10 
+	
+
+}//FIN DE LA FUNCIÓN
