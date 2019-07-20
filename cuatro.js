fc71364ace39a30648751f64786a8a81c2805ddf
Ivana k <52494488+ivanakruk@users.noreply.github.com>
2019-07-20 11:28:00 -0300

    ejercicio 4 del rec, arreglado

--- cuatro.js
+++ cuatro.js
@@ -1,43 +1,46 @@
-/*(IF)Pedir dos números y mostrar el resultado: 
-Si son iguales los muestro concatenados. 
-Si el primero es mayor, los divido, 
-de lo contrario los sumo. 
-Si la suma es menor a 50 ,además de mostrar el resultado, muestro el mensaje 
-"la suma es xxx y es menor a 50".*/
+    /*(IF)Pedir dos números y mostrar el resultado: 
+    Si son iguales los muestro concatenados. 
+    Si el primero es mayor, los divido, 
+    de lo contrario los sumo. 
+    Si la suma es menor a 50 ,además de mostrar el resultado, muestro el mensaje 
+    "la suma es xxx y es menor a 50".*/
 
-function mostrar()
-{
-    var numA;
-    var numB;
-    var resultado;
-
-    numA=prompt("Ingrese el primer numero");
-    numB=prompt("Ingrese el segundo numero");
-    
-    
-    if (numA==numB)
+    function mostrar()
     {
-      resultado=numA+numB;  
-    }
-    else
-    {             
-        numA=parseInt(numA);
-        numB=parseInt(numB);
+        var numA;
+        var numB;
+        var resultado;
 
-        if(numA>numB)
+        numA=prompt("Ingrese el primer numero");
+        numB=prompt("Ingrese el segundo numero");
+        
+        
+        if (numA==numB)
         {
-            resultado=numA/numB;               
+        resultado=numA+numB;  
         }
         else
-        {
-            resultado=numA+numB;
-        }       
+        {             
+            numA=parseInt(numA);
+            numB=parseInt(numB);
 
-    }  
-   if(resultado<50)
-    {
-        resultado=numA+numB+(" es el y es menor a 50");
+            if(numA>numB)
+            {
+                resultado=numA/numB;               
+            }
+            else
+            {
+                resultado=numA+numB;
+
+
+                if(resultado<50)
+                {
+                    resultado=numA+numB+(" es la suma y es menor a 50");
+                }
+            }           
+
+        }  
+    
+    alert(resultado);
+
     }
-alert(resultado);
-
-}
