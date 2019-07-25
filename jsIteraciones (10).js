83b605c70e82f3c86a373fce3b480fc5198819e1
Ivana k <52494488+ivanakruk@users.noreply.github.com>
2019-07-25 19:46:04 -0300

    WHILE EJR 10 ARREGLADO

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -1,86 +1,85 @@
 function mostrar()
-{	
-	var contNegativos=0;
-	var contPositivo=0;
-	var contPares=0
-	var contCeros=0
-	var numero;
-	var promediosPositivos;
-	var promediosNegativos;
-	var acumPositivo=0;
-	var acumNegativo=0;//SI NO LO IGUALO A CERO O A 1 NO VA ACUMULAR Y VA DAR UNDEFINED o NaN, undefine mas algo NaN
-	//declarar contadores y variables 
+{ /*Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
+	1-Suma de los negativos.2-Suma de los positivos.3-Cantidad de positivos.
+	4-Cantidad de negativos.5-Cantidad de ceros. 6-Cantidad de números pares. 
+	7-Promedio de positivos. 8-Promedios de negativos. 9-Diferencia entre positivos y negativos, (positvos-negativos).*/
+		var numero;
+		var contadorCeros=0;
+		var contadorPares=0;
+		var contadorPositivos=0;
+		var contadorNegativos=0;
+		var acumuladorPositivos=0;
+		var acumuladorNegativos=0;//se inicializa en 0, sino me da undefined o NaN;
+		var promedioPositivos;
+		var promedioNegativos;
+		var respuesta=true;
+		var Diferencia;
+		while(respuesta==true)
+		{
+			numero=prompt("Ingrese un numero");
+			numero=parseInt(numero);
 	
-	var respuesta="true";
-
-	while(respuesta=="true")
-	{
-		numero=prompt("ingrese un numero");
-		numero=parseInt(numero);
-		
-
-
-		if(numero>0)
-		{
-			contPositivo++;    // O contadorPositivo=contadorPositivo + 1
-			acumPositivo=acumPositivo+=numero; 
-		}
-		
-		else
-		{
-			
-			
-			if(numero<0)
+			if(numero>0)
 			{
-			
-				contNegativos++; 
-				acumPositivo=acumPositivo+=numero;
-
+				contadorPositivos++;
+				acumuladorPositivos+=numero;
 			}
 			else
 			{
-				contCeros++;
+				if(numero<0)
+				{
+					contadorNegativos++;
+					acumuladorNegativos+=numero;
+				}
+				else
+				{
+					contadorCeros++;
+				}
 			}
-
-
-		}
+			
+			
+			if(numero%2==0)
+			{
+				contadorPares++;
+			}
+	
 		
-		if (numero%2==0)
+		
+		if(contadorPositivos!=0)
 		{
-			contPares++;
+			promedioPositivos=acumuladorPositivos/contadorPositivos;
+		}
+			else
+		{	
+				promedioPositivos=("No se ingresaron numeros Positivos");
+		}
+		if(contadorNegativos!=0)
+		{
+	
+			promedioNegativos=acumuladorNegativos/contadorNegativos;//si se divide por 0 da NaN
+		}
+			else
+		{
+				promedioNegativos=("No se ingresaron numeros Negativos");
 		}
 		
-		respuesta=confirm("Desea ingresar otro numero");
-	
+				
+		
+		
+		respuesta=confirm("Desea continuar?");
+		
 	}
 
-	if(contPositivo!=0)
-	{
-		promediosPositivos=acumPositivo/contPositivo;
-	}
-	else
-	{
-		promediosPositivos="No se ingresaron numeros positivos";
-	}
-	if(contNegativos!=0)
-	{
-		promediosNegativos=acumNegativo/contNegativos;
-	}
-	else
-	{
-		promediosNegativos="No se ingresaron numeros negativos"
-	}
+	Diferencia=acumuladorPositivos+(acumuladorNegativos);
 	
 	
-
-	document.write("Suma de los Positivos: " + acumPositivo)+"<br>";
-	document.write("Suma de los Negativos: " + acumNegativo)+"<br>";
-	document.write("Cantidad de Positivos: " + contPositivo)+"<br>";
-	document.write("Cantidad de Negativos: " + contNegativos)+"<br>";
-	document.write("Cantidad de Ceros: " + contCeros)+"<br>";
-	document.write("Cantidad de Pares: " + contPares)+"<br>";
-	document.write("Promedio de Positivos: " + promedioPositivo)+"<br>";
-	document.write("Promedio de Negativos: " + promedioNegativos)+"<br>";
-
-
-}//FIN DE LA FUNCIÓN
+	document.write("1.suma de positivos :"+acumuladorPositivos + "<br>");
+	document.write("2.suma de negativoss :"+acumuladorNegativos + "<br>");
+	document.write("3.cantidad de positivos :"+contadorPositivos + "<br>");
+	document.write("4.cantidad de negativos :"+contadorNegativos + "<br>");
+	document.write("4.cantidad de ceros :"+contadorCeros + "<br>");
+	document.write("5.cantidad de pares :"+contadorPares + "<br>");
+	document.write("7.Promedio de positivos :"+promedioPositivos+ "<br>");
+	document.write("8.Promedio de negativos:"+promedioNegativos + "<br>");
+	document.write("9.Diferencia e/ positivos y negativos: "+ Diferencia + "<br>");
+	}//FIN DE LA FUNCIÓN
